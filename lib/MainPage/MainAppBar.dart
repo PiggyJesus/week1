5aa54d2c340e0455d299ac3132bb8f6f10e54446
PiggyJesus <Kulakov.Stanislav82@gmail.com>
2023-03-23 16:28:34 +0300

    Initial commit

---
+++ lib/MainPage/MainAppBar.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class MainAppBar extends StatelessWidget {
+  const MainAppBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+        leading: Column(
+          children: const <Widget>[
+            Text("data")
+          ],
+        )
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'MainPage/MyApp.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
