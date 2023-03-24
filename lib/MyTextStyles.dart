6ec029bb58e9055deddaedeb4d9e902b8d843c41
PiggyJesus <Kulakov.Stanislav82@gmail.com>
2023-03-24 17:51:31 +0300

    TextStyles - height sdelan statusBar ne nujen

--- lib/MyTextStyles.dart
+++ lib/MyTextStyles.dart
@@ -5,64 +5,64 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 abstract class MyTextStyles{
   static final appBar = TextStyle(
     fontFamily: "OpenSans",
-    fontSize: 17.sp,
+    fontSize: 17.h,
     color: Colors.black,
     fontWeight: FontWeight.w600,
-    //height: 22.sp,
+    height: 22/17,
     letterSpacing: -0.41.w,
   );
 
   static final name = TextStyle(
     fontFamily: "OpenSans",
-    fontSize: 18.sp,
+    fontSize: 18.h,
     color: Colors.black,
     fontWeight: FontWeight.w400,
-    //height: 25.h,
+    height: 25/18,
     letterSpacing: -0.24.w,
   );
 
   static final address = TextStyle(
     fontFamily: "OpenSans",
-    fontSize: 13.sp,
+    fontSize: 13.h,
     color: Color.fromRGBO(0, 122, 255, 1),
     fontWeight: FontWeight.w400,
-    //height: 18.h,
+    height: 18/13,
     letterSpacing: -0.08.w,
   );
 
   static final price = TextStyle(
     fontFamily: "OpenSans",
-    fontSize: 16.sp,
+    fontSize: 16.h,
     color: Color.fromRGBO(0, 0, 0, 1),
     fontWeight: FontWeight.w600,
-    //height: 20.h,
+    height: 20/16,
     letterSpacing: -0.24.w,
   );
 
   static final descriptionHead = TextStyle(
     fontFamily: "OpenSans",
-    fontSize: 18.sp,
+    fontSize: 18.h,
     color: Color.fromRGBO(0, 0, 0, 1),
     fontWeight: FontWeight.w700,
-    //height: 15.h,
+    height: 15/18,
     letterSpacing: 0.35.w,
   );
 
   static final description = TextStyle(
     fontFamily: "OpenSans",
-    fontSize: 12.sp,
+    fontSize: 12.h,
     color: Color.fromRGBO(0, 0, 0, 1),
     fontWeight: FontWeight.w400,
-    //height: 16.8.h,
+    height: 16.8/12,
     letterSpacing: 0.35.w,
   );
 
   static final podrobnee = TextStyle(
     fontFamily: "OpenSans",
-    fontSize: 12.sp,
+    fontSize: 12.h,
     color: Color.fromRGBO(0, 122, 255, 1),
     fontWeight: FontWeight.w400,
-    //height: 22.h,
+    height: 22/12,
     letterSpacing: -0.41.w,
   );
 }
